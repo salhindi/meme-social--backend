dbd13113f218f4382d400d46dccd4d1b2867f57e
salhindi <calisaraalhindi@gmail.com>
2021-10-20 07:54:41 -0700

    index, create, show, destroy methods

--- app/controllers/api/v1/memes_controller.rb
+++ app/controllers/api/v1/memes_controller.rb
@@ -1,18 +1,30 @@
 class Api::V1::MemesController < ApplicationController
     def index
+        memes = Meme.all
+        render json: memes
     end
 
     def create
+        meme = Meme.new(meme_params)
+        if meme.save
+            render json: meme
+        else
+            render json: {error; 'AW SNAP SOMETHINGGS WRONG'}
     end
 
     def show
+        meme = Meme.find(params[:id])
+        render json: meme
     end
 
     def destroy
+        meme = Meme.find(params[:id])
+        meme.destroy
     end
 
     private
 
     def meme_params
+        params.require(:meme).permit(:title, :top_text, :bottom_text, :image_id)
     end
 end
