4f810994c17d959194e3282d05452cab60aa51cc
salhindi <calisaraalhindi@gmail.com>
2021-10-20 07:45:17 -0700

    added controller methods

--- app/controllers/api/v1/memes_controller.rb
+++ app/controllers/api/v1/memes_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::MemesController < ApplicationController
+    def index
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    private
+
+    def meme_params
+    end
 end
--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,7 +1,9 @@
 class Meme < ApplicationRecord
-    has_many_attached :image
+    has_many_attached :image, dependent: :destroy
     has_many :tags
     
     validates :title, presence: true
+
+
     # validates_attachment :image, presence: true, content_type: ["image/jpeg", "image/gif", "image/png"]
 end
