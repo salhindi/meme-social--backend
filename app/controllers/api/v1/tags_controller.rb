4d4e900b677ffed2958947c04948a22e340cbfd0
salhindi <calisaraalhindi@gmail.com>
2021-10-20 07:57:46 -0700

    index, create, show, destroy tag

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,3 +1,31 @@
 class Api::V1::TagsController < ApplicationController
-    
+    def index
+        tags = Tag.all
+        render json: tags
+    end
+
+    def create
+        tag = Tag.new(tag_params)
+        if tag.save
+            render json: tag
+        else
+            render json: {error; 'AW SNAP SOMETHINGGS WRONG'}
+    end
+
+    def show
+        tag = Tag.find(params[:id])
+        render json: tag
+    end
+
+    def destroy
+        tag = Tag.find(params[:id])
+        tag.destroy
+    end
+
+    private
+
+    def tag_params
+        params.require(:tag).permit(:title, :top_text, :bottom_text, :image_id)
+    end
+
 end
